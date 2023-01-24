73b0504cdbd033c50608506a07491bca35c55e2b
AdrianSchulze <adrian@sei-sicherer.de>
2023-01-24 17:18:39 +0100

    Feat: - display Dialog with dark background when click on "add channel" - added "Metric" class - adjusted Channel class

--- backend/src/main/java/de/neuefische/backend/metric/Metric.java
+++ backend/src/main/java/de/neuefische/backend/metric/Metric.java
@@ -1,4 +1,22 @@
 package de.neuefische.backend.metric;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Metric {
+    @Id
+    private String id;
+    private String channelId;
+    private Date date;
+    private int impressions;
+    private int clicks;
+    private double cost;
+    private double conversions;
 }
