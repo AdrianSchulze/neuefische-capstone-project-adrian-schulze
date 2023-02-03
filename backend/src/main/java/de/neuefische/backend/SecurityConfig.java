d13432c855bb7665e490b6dbe98e63c87178c664
AdrianSchulze <adrian@sei-sicherer.de>
2023-02-03 10:54:47 +0100

    fix: - sonar fix

--- backend/src/main/java/de/neuefische/backend/SecurityConfig.java
+++ backend/src/main/java/de/neuefische/backend/SecurityConfig.java
@@ -22,19 +22,19 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-
+        String basic = "BASIC";
         return http
                 .csrf().disable()
                 .httpBasic().and()
                 .authorizeHttpRequests()
                 .antMatchers(HttpMethod.POST, "/api/users/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/users/signup").permitAll()
-                .antMatchers(HttpMethod.POST, "/api/metrics").hasRole("BASIC")
-                .antMatchers(HttpMethod.POST, "/api/channels").hasRole("BASIC")
-                .antMatchers(HttpMethod.DELETE, "/api/metrics").hasRole("BASIC")
-                .antMatchers(HttpMethod.DELETE, "/api/channels").hasRole("BASIC")
-                .antMatchers(HttpMethod.GET, "/api/metrics").hasRole("BASIC")
-                .antMatchers(HttpMethod.GET, "/api/channels").hasRole("BASIC")
+                .antMatchers(HttpMethod.POST, "/api/metrics").hasRole(basic)
+                .antMatchers(HttpMethod.POST, "/api/channels").hasRole(basic)
+                .antMatchers(HttpMethod.DELETE, "/api/metrics").hasRole(basic)
+                .antMatchers(HttpMethod.DELETE, "/api/channels").hasRole(basic)
+                .antMatchers(HttpMethod.GET, "/api/metrics").hasRole(basic)
+                .antMatchers(HttpMethod.GET, "/api/channels").hasRole(basic)
                 .anyRequest()
                 .authenticated()
                 .and()
